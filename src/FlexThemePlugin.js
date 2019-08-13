64e6ccaf2d46b01f9c15bb542235019df07b30df
sturg3576 <53346347+sturg3576@users.noreply.github.com>
2019-08-13 09:51:56 -0400

    Add files via upload
    uploaded manually.  FlexThemePlugin

--- src/FlexThemePlugin.js
+++ src/FlexThemePlugin.js
@@ -19,15 +19,67 @@ export default class FlexThemePlugin extends FlexPlugin {
    * @param flex { typeof import('@twilio/flex-ui') }
    * @param manager { import('@twilio/flex-ui').Manager }
    */
-  init(flex, manager) {
-    this.registerReducers(manager);
-
-    const options = { sortOrder: -1 };
-    flex.AgentDesktopView
-      .Panel1
-      .Content
-      .add(<CustomTaskListContainer key="demo-component" />, options);
-  }
+   init(flex, manager) {
+     // This line tells Flex where to find the logo that renders in the top left corner
+     flex.MainHeader.defaultProps.logoUrl = "https://i2.wp.com/solomonconsult.com/wp-content/uploads/2019/05/solomonconsulting_logo_199x75px.png?fit=199%2C75&ssl=1"
+     // The configuration object describes what the different Flex components and subcomponents should look like
+     const configuration = {
+       colorTheme: {
+         colors: {
+           userAvailableColor: "#00b894",
+           acceptColor: "#36D576",
+           declineColor: "#F22F46"
+         },
+         overrides: {
+           IncomingTaskCanvas: {
+             Container: {
+               background: "#0984e3"
+             },
+           },
+           MainHeader: {
+               Container: {
+                   background: "#0D122B",
+                   borderColor: "#fff"
+               }
+           },
+           SideNav: {
+               Container: {
+                   background: "#0D122B",
+                   borderColor: "#fff"
+               },
+               Button: {
+                 background: "#F22F46"
+               }
+           },
+           NoTasksCanvas: {
+             Container: {
+               background: "#fff"
+             },
+           },
+           UserActivityControls: {
+               Item: {
+                   lightHover: false
+               },
+               Items: {
+                   background: "#F22F46",
+               }
+           },
+           TaskList: {
+             Container: {
+               background: "#fff"
+             },
+             Item: {
+               Icon: {
+                 background: "#0984e3"
+               }
+             }
+           }
+         }
+       }
+     }
+     // Finally, we pass the configuration to the Flex manager to make the changes go live
+     manager.updateConfig(configuration)
+   }
 
   /**
    * Registers the plugin reducers
